595f1c5298dfd77f0d082719f09234e162a9ef0c
ascandone <alescandone@gmail.com>
2024-09-27 16:43:13 +0200

    handling simple routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,66 @@
-import { FC, useState } from "react";
+import { FC, useCallback, useEffect, useState } from "react";
 import { BetaReducibleTerm, TermsList } from "./components/ReducibleTerm";
-import { Editor } from "./components/Editor";
+import { Editor, EditorProps } from "./components/Editor";
 import { Program } from "./lambda/ast";
 import { unalias } from "./lambda/semantics";
+
+const useRouter = () => {
+  // hack to re-render the component
+  const [, setCounter] = useState(0);
+  const refresh = useCallback(() => {
+    setCounter((counter) => counter + 1);
+  }, [setCounter]);
+
+  useEffect(() => {
+    window.addEventListener("popstate", refresh);
+    return () => window.removeEventListener("popstate", refresh);
+  }, [refresh]);
+
+  return {
+    location: window.location,
+    push(url: string) {
+      history.pushState("", "", url);
+      refresh();
+    },
+    redirect(url: string) {
+      history.replaceState("", "", url);
+      refresh();
+    },
+  };
+};
 
 const App: FC = () => {
   const [program, setProgram] = useState<Program | undefined>(undefined);
 
-  return program === undefined ? (
-    <Editor onSubmit={setProgram} />
-  ) : (
-    <div className="px-4 mx-auto w-full">
-      <div className="mx-auto flex justify-center py-32">
-        <TermsList term={unalias(program)} />
-      </div>
-    </div>
-  );
+  const router = useRouter();
+  const handleSubmit: EditorProps["onSubmit"] = (program) => {
+    setProgram(program);
+    router.push("/steps");
+  };
+
+  useEffect(() => {
+    if (router.location.pathname === "/steps" && program === undefined) {
+      router.redirect("/");
+    }
+  }, [router, program]);
+
+  switch (router.location.pathname) {
+    case "/steps":
+      if (program === undefined) {
+        return null;
+      }
+
+      return (
+        <div className="px-4 mx-auto w-full">
+          <div className="mx-auto flex justify-center py-32">
+            <TermsList term={unalias(program)} />
+          </div>
+        </div>
+      );
+
+    default:
+      return <Editor onSubmit={handleSubmit} />;
+  }
 };
 
 export const Example: FC = () => {

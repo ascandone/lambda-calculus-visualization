9d19c3aaec286028127cf64d4857b5fedd64ffc5
ascandone <alescandone@gmail.com>
2024-09-27 12:29:30 +0200

    tweak whitespace

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,11 @@ import { BetaReducibleTerm, Pre } from "./components/ReducibleTerm";
 const s = String.raw`(\x y z. x z (y z))`;
 const k = String.raw`(\u v. u)`;
 // const i = String.raw`(\t . t)`;
-// const comb = String.raw`(${s} (${k} ${s})) ${k}`;
-const comb = String.raw`${s} ${k} ${k}`;
+const comb = String.raw`(${s} (${k} ${s})) ${k}`;
 
 const App: FC = () => (
   <div className="px-4 mx-auto w-full h-screen bg-gray-50">
-    <div className="h-24"></div>
-    <div className="mx-auto flex justify-center">
+    <div className="mx-auto flex justify-center py-32">
       <RenderTermList src={comb} />
       {/* <RenderTermList src={String.raw`(\x y. (\a.a) b)`} /> */}
     </div>
